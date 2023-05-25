6ac4867c623f9fe805b34afea1576c05f57a1d95
Luigi <114937172+luigidesu@users.noreply.github.com>
2023-05-24 20:44:50 -0600

    Update MetaDeleter9000.py
    It now accepts many files, instead of just 1

--- MetaDeleter9000.py
+++ MetaDeleter9000.py
@@ -1,28 +1,28 @@
 #This script gets rid of all your images metadata.
 #But, can't I do that in Photoshop already?
-#Yes, but it is still some metadata there, like your Photoshop version and such, and I'm a paranoic son of a bitch who doesn't like to get tracked down, so I want to get rid of ALL MY IMAGES METADATA
-#So yeah, it gets rid of the meta.
+#Yes, but it is still some metadata there, like your Photoshop version and such, and I'm a paranoic son of a bitch who doesn't like to get tracked down, so I want to get rid of all the metadata
 from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
 import os
 
-def delete_metadata_file():
+def delete_metadata_files():
     try:
         # Opens a dialog to select the files.
         root = tk.Tk()
         root.withdraw()
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.PNG *.JPG *.JPEG *.GIF")])
+        file_paths = filedialog.askopenfilenames(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif")])
         
-        if file_path:
-            delete_metadata(file_path)
-            print("All metadata extermined.")
+        if file_paths:
+            for file_path in file_paths:
+                delete_metadata(file_path)
+            print("metadata successfully bonked.")
         else:
-            print("No file selected.")
+            print("select some files, come on.")
     except FileNotFoundError as e:
         print("File not found.")
     except IsADirectoryError as e:
-        print("Selected path is a directory. Please select an image file.")
+        print("get yo folder outta here, I only accept individual files")
     except Exception as e:
         print(f"Error: {e}")
 
@@ -30,20 +30,20 @@ def delete_metadata(image_path):
     try:
         file_size = os.path.getsize(image_path)
         if file_size == 0:
-            print("Empty file. Metadata cannot be extermined.")
+            print(f"Yo files are 0 bytes, stupidhead.")
             return
 
         image = Image.open(image_path)
 
-        # Creates a copy of the image without metadata
+        # Creates a copy of the image to work on
         image_without_metadata = image.copy()
         image_without_metadata.info = {}
 
         # Overwrites the original image with the cleaned one.
         image_without_metadata.save(image_path)
 
-        print(f"Metadata successfully deleted for {image_path}")
+        print(f"Metadata successfully bonked for {image_path}")
     except Exception as e:
         print(f"Error: {e}")
 
-delete_metadata_file()
+delete_metadata_files()
